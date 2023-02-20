0c851c2eab381b0fc64f3728c61649982d222415
Guillaume Saile <gsaile@k0r2p5.42mulhouse.fr>
2023-02-20 16:39:12 +0100

    [M01][ex06] Done and [M01][ex05] Remove cout from constructor and destructor

--- M01/ex06/main.cpp
+++ M01/ex06/main.cpp
@@ -6,24 +6,36 @@
 /*   By: gsaile <gsaile@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 15:52:09 by gsaile            #+#    #+#             */
-/*   Updated: 2023/02/20 16:37:27 by gsaile           ###   ########.fr       */
+/*   Updated: 2023/02/20 16:36:45 by gsaile           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Harl.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-	Harl harl;
-	std::cout << "Complain with DEBUG:" << std::endl;
-	harl.complain("DEBUG");
-	std::cout << std::endl << "Complain with INFO:" << std::endl;
-	harl.complain("INFO");
-	std::cout << std::endl << "Complain with WARNING:" << std::endl;
-	harl.complain("WARNING");
-	std::cout << std::endl << "Complain with ERROR:" << std::endl;
-	harl.complain("ERROR");
-	std::cout << std::endl << "Complain with something else:" << std::endl;
-	harl.complain("OFD");
+	Harl	harl;
+	int		minLevel = -1;
+	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	if (argc != 2)
+		minLevel = 0;
+	else
+		for (int i = 0; i < 4; i++)
+			if (argv[1] == levels[i])
+				minLevel = i;
+	switch(minLevel)
+	{
+		case 0:
+			harl.complain("DEBUG");
+		case 1:
+			harl.complain("INFO");
+		case 2:
+			harl.complain("WARNING");
+		case 3:
+			harl.complain("ERROR");
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
