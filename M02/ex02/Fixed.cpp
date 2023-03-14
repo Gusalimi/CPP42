ab8f85d8e9f6b11fd75a33520c397f6a12c6c6cc
Guillaume Saile <gsaile@k0r3p4.42mulhouse.fr>
2023-03-14 16:14:37 +0100

    [M02][ex02] WIP

---
+++ M02/ex02/Fixed.cpp
@@ -0,0 +1,150 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsaile <gsaile@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 11:34:15 by gsaile            #+#    #+#             */
+/*   Updated: 2023/03/14 16:04:20by gsaile           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	ft_pow(int base, int exp)
+{
+	int	result = 1;
+	for (int i = 0; i < exp; i++)
+		result *= base;
+	return (result);
+}
+
+const int Fixed::_fractBits = 8;
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->_value = 0;
+}
+
+Fixed::Fixed( Fixed const &b )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = b;
+}
+
+Fixed::Fixed( int const value )
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = value << Fixed::_fractBits;
+}
+
+Fixed::Fixed( float const value )
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = roundf(value * ft_pow(2, Fixed::_fractBits));
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int		Fixed::getRawBits( void ) const
+{
+	return (this->_value);
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+	this->_value = raw;
+}
+
+int	Fixed::toInt( void ) const
+{
+	return (this->_value >> Fixed::_fractBits);
+}
+
+#include <bitset>
+float	Fixed::toFloat( void ) const
+{
+	float result;
+	result = (1.0f * (int)this->_value) / ft_pow(2, Fixed::_fractBits);
+	return (result); 
+}
+
+Fixed & Fixed::operator=( Fixed const &rhs )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->_value = rhs.getRawBits();
+	return *this;
+}
+
+bool Fixed::operator>( Fixed const &rhs )
+{
+	return (this->toFloat() > rhs.toFloat());
+}
+
+bool Fixed::operator<( Fixed const &rhs )
+{
+	return (this->toFloat() < rhs.toFloat());
+}
+
+bool Fixed::operator>=( Fixed const &rhs )
+{
+	return (this->toFloat() >= rhs.toFloat());
+}
+
+bool Fixed::operator<=( Fixed const &rhs )
+{
+	return (this->toFloat() <= rhs.toFloat());
+}
+
+bool Fixed::operator==( Fixed const &rhs )
+{
+	return (this->toFloat() == rhs.toFloat());
+}
+
+bool Fixed::operator!=( Fixed const &rhs )
+{
+	return (this->toFloat() != rhs.toFloat());
+}
+
+Fixed Fixed::operator+( Fixed const &rhs )
+{
+	return (this->toFloat() + rhs.toFloat());
+}
+
+Fixed Fixed::operator-( Fixed const &rhs )
+{
+	return (this->toFloat() - rhs.toFloat());
+}
+
+Fixed Fixed::operator*( Fixed const &rhs )
+{
+	return (this->toFloat() * rhs.toFloat());
+}
+
+Fixed Fixed::operator/( Fixed const &rhs )
+{
+	return (this->toFloat() / rhs.toFloat());
+}
+
+Fixed & Fixed::operator++()
+{
+	++this->_value; 
+	return (*this);
+}
+
+Fixed & Fixed::operator++(int i)
+{
+	(void)i;
+	this->_value++; 
+	return (*this);
+}
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs )
+{
+	o << rhs.toFloat();
+	return (o);
+}
---
+++ M02/ex02/Fixed.hpp
@@ -0,0 +1,41 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+int	ft_pow(int base, int exp);
+
+class Fixed
+{
+public:
+	Fixed();
+	Fixed( Fixed const &b );
+	Fixed( int const value );
+	Fixed( float const value );
+	~Fixed();
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+	Fixed & operator=( Fixed const &rhs );
+	bool operator>( Fixed const &rhs );
+	bool operator<( Fixed const &rhs );
+	bool operator>=( Fixed const &rhs );
+	bool operator<=( Fixed const &rhs );
+	bool operator==( Fixed const &rhs );
+	bool operator!=( Fixed const &rhs );
+	Fixed operator+( Fixed const &rhs );
+	Fixed operator-( Fixed const &rhs );
+	Fixed operator*( Fixed const &rhs );
+	Fixed operator/( Fixed const &rhs );
+	Fixed & operator++();
+	Fixed & operator++(int);
+private:
+	int					_value;
+	static const int	_fractBits;
+};
+
+std::ostream & operator<<( std::ostream & o, Fixed const & rhs );
+
+#endif /* FIXED_HPP */
