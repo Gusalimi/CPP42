1a0eeba433858158526287e2afb0514c8bd9dc7b
Guillaume Saile <gsaile@k0r2p4.42mulhouse.fr>
2023-04-28 15:52:08 +0200

    [M04][ex03] MateriaSource init

---
+++ M04/ex03/MateriaSource.cpp
@@ -0,0 +1,16 @@
+#include "MateriaSource.hpp"
+
+MateriaSource::MateriaSource()
+{
+	
+}
+
+MateriaSource::MateriaSource(const MateriaSource& b)
+
+MateriaSource::~MateriaSource()
+
+MateriaSource& MateriaSource::operator=(const MateriaSource& rhs)
+
+void MateriaSource::learnMateria(AMateria*)
+
+AMateria* MateriaSource::createMateria(std::string const & type)
--- M04/ex03/MateriaSource.hpp
+++ M04/ex03/MateriaSource.hpp
@@ -1,4 +1,19 @@
 #ifndef MATERIA_SOURCE_HPP
 #define MATERIA_SOURCE_HPP
 
+#include "IMateriaSource.hpp"
+
+class MateriaSource: public IMateriaSource
+{
+public:
+	MateriaSource();
+	MateriaSource(const MateriaSource& b);
+	~MateriaSource();
+	MateriaSource& operator=(const MateriaSource& rhs);
+
+	void learnMateria(AMateria*);
+	AMateria* createMateria(std::string const & type);
+};
+
+
 #endif //MATERIA_SOURCE_HPP
