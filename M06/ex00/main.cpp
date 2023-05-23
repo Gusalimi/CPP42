f50917c219f20ad267d5a18e077bcc096027b0d2
Guillaume Saile <gsaile@k0r1p4.42mulhouse.fr>
2023-05-23 15:29:18 +0200

    [M06][ex00] Seems to work

--- M06/ex00/main.cpp
+++ M06/ex00/main.cpp
@@ -34,6 +34,132 @@ int isANumber(std::string s)
 		return (3);
 }
 
+void fromChar(std::string s)
+{
+	char c = s[0];
+	if (c < 32 || c == 127)
+		std::cout << "char: Non displayable" << std::endl;
+	else
+		std::cout << "char: '" << c  << "'" << std::endl;
+	std::cout << "int: " << static_cast<int>(c) << std::endl;
+	std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+}
+
+void fromInt(std::string s)
+{
+	try
+	{
+		int i = std::stoi(s);
+		std::cout << "char: ";
+		if (i < 0 || i > 127)
+			std::cout << "impossible" << std::endl;
+		else if (i < 32 || i == 127)
+			std::cout << "Non displayable" << std::endl;
+		else
+			std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
+		std::cout << "int: " << i << std::endl;
+		std::cout << "float: " << i << ".0f" << std::endl;
+		std::cout << "double: " << i << ".0" << std::endl;
+	}
+	catch (const std::out_of_range& e)
+	{
+		std::cerr << "Number out of range" << std::endl;
+	}
+}
+
+void fromDouble(std::string s)
+{
+	try
+	{
+		double d = std::stod(s);
+		std::cout << "char: ";
+		if (d < 0 || d > 127 || s == "nan")
+			std::cout << "impossible" << std::endl;
+		else if (d < 32 || d == 127)
+			std::cout << "Non displayable" << std::endl;
+		else
+			std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
+		try
+		{
+			int i = std::stoi(s);
+			std::cout << "int: " << i << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << "int: impossible" << std::endl;
+		}
+		try
+		{
+			float f = std::stof(s);
+			std::cout << "float: ";
+			if (s == "+inf")
+				std::cout << "+";
+			std::cout << f;
+			if (f - static_cast<int>(f) == 0)
+				std::cout << ".0";
+			std::cout << "f" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << "float: impossible" << std::endl;
+		}
+		std::cout << "double: ";
+		if (s == "+inf")
+			std::cout << "+";
+		std::cout << d;
+		if (d - static_cast<int>(d) == 0)
+			std::cout << ".0";
+		std::cout << std::endl;
+	}
+	catch (const std::out_of_range& e)
+	{
+		std::cerr << "Number out of range" << std::endl;
+	}
+}
+
+void fromFloat(std::string s)
+{
+	try
+	{
+		float f = std::stof(s);
+		std::cout << "char: ";
+		if (f < 0 || f > 127 || s == "nanf")
+			std::cout << "impossible" << std::endl;
+		else if (f < 32 || f == 127)
+			std::cout << "Non displayable" << std::endl;
+		else
+			std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
+		try
+		{
+			int i = std::stoi(s);
+			std::cout << "int: " << i << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << "int: impossible" << std::endl;
+		}
+		std::cout << "float: ";
+		if (s == "+inff")
+			std::cout << "+";
+		std::cout << f;
+		if (f - static_cast<int>(f) == 0)
+			std::cout << ".0";
+		std::cout << "f" << std::endl;
+		std::cout << "double: ";
+		if (s == "+inff")
+			std::cout << "+";
+		std::cout << static_cast<double>(f);
+		if (f - static_cast<int>(f) == 0)
+			std::cout << ".0";
+		std::cout << std::endl;
+	}
+	catch (const std::out_of_range& e)
+	{
+		std::cerr << "Number out of range" << std::endl;
+	}
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -43,102 +169,13 @@ int main(int argc, char **argv)
 	}
 	std::string s = argv[1];
 	if (s.length() == 1 && !std::isdigit(s[0]))
-	{
-		char c = s[0];
-		if (c < 32 || c == 127)
-			std::cout << "char: Non displayable" << std::endl;
-		else
-			std::cout << "char: '" << c  << "'" << std::endl;
-		std::cout << "int: " << static_cast<int>(c) << std::endl;
-		std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
-		std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
-	}
+		fromChar(s);
 	else if (isANumber(s) == 1)
-	{
-		try
-		{
-			int i = std::stoi(s);
-			std::cout << "char: ";
-			if (i < 0 || i > 127)
-				std::cout << "impossible" << std::endl;
-			else if (i < 32 || i == 127)
-				std::cout << "Non displayable" << std::endl;
-			else
-				std::cout << "'" << static_cast<char>(i) << "'" << std::endl;
-			std::cout << "int: " << i << std::endl;
-			std::cout << "float: " << i << ".0f" << std::endl;
-			std::cout << "double: " << i << ".0" << std::endl;
-		}
-		catch (const std::out_of_range& e)
-		{
-			std::cerr << "Number out of range" << std::endl;
-		}
-	}
+		fromInt(s);
 	else if (isANumber(s) == 2 || s == "-inf" || s == "+inf" || s == "nan")
-	{
-		try
-		{
-			double d = std::stod(s);
-			std::cout << "char: ";
-			if (d < 0 || d > 127)
-				std::cout << "impossible" << std::endl;
-			else if (d < 32 || d == 127)
-				std::cout << "Non displayable" << std::endl;
-			else
-				std::cout << "'" << static_cast<char>(d) << "'" << std::endl;
-			if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min())
-				std::cout << "int: impossible" << std::endl;
-			else
-				std::cout << "int: " << static_cast<int>(d) << std::endl;
-			if (d > std::numeric_limits<float>::max() || d < std::numeric_limits<float>::min())
-				std::cout << "float: impossible" << std::endl;
-			else
-			{
-				std::cout << "float: " << static_cast<float>(d);
-				if (d - static_cast<int>(d) == 0)
-					std::cout << ".0";
-				std::cout << "f" << std::endl;
-			}
-			std::cout << "double: " << d;
-			if (d - static_cast<int>(d) == 0)
-				std::cout << ".0";
-			std::cout << std::endl;
-		}
-		catch (const std::out_of_range& e)
-		{
-			std::cerr << "Number out of range" << std::endl;
-		}
-	}
+		fromDouble(s);
 	else if (isANumber(s) == 3 || s == "-inff" || s == "+inff" || s == "nanf")
-	{
-		try
-		{
-			float f = std::stof(s);
-			std::cout << "char: ";
-			if (f < 0 || f > 127)
-				std::cout << "impossible" << std::endl;
-			else if (f < 32 || f == 127)
-				std::cout << "Non displayable" << std::endl;
-			else
-				std::cout << "'" << static_cast<char>(f) << "'" << std::endl;
-			if (f > std::numeric_limits<int>::max() || f < std::numeric_limits<int>::min())
-				std::cout << "int: impossible" << std::endl;
-			else
-				std::cout << "int: " << static_cast<int>(f) << std::endl;
-			std::cout << "float: " << f;
-			if (f - static_cast<int>(f) == 0)
-				std::cout << ".0";
-			std::cout << "f" << std::endl;
-			std::cout << "double: " << static_cast<double>(f);
-			if (f - static_cast<int>(f) == 0)
-				std::cout << ".0";
-			std::cout << std::endl;
-		}
-		catch (const std::out_of_range& e)
-		{
-			std::cerr << "Number out of range" << std::endl;
-		}
-	}
+		fromFloat(s);
 	else
 		std::cerr << "Unrecognised format" << std::endl;
 }
