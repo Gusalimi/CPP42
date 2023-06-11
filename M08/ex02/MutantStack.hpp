fcd8a5663ce42bf518ac2436ee2ee68f70250dab
Guillaume Saile <gsaile@k2r3p2.42mulhouse.fr>
2023-06-11 18:26:45 +0200

    [M08] Done

--- M08/ex02/MutantStack.hpp
+++ M08/ex02/MutantStack.hpp
@@ -15,9 +15,14 @@ public:
 		return *this;
 	};
 	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
     iterator begin() { return std::stack<T>::c.begin(); }
     iterator end() { return std::stack<T>::c.end(); }
+    reverse_iterator rbegin() { return std::stack<T>::c.rbegin(); }
+    reverse_iterator rend() { return std::stack<T>::c.rend(); }
 };
 
 #endif /* MUTANTSTACK_HPP */
--- M08/ex02/main.cpp
+++ M08/ex02/main.cpp
@@ -4,51 +4,120 @@
 
 int main()
 {
-	std::cout << "MutantStack:" << std::endl;
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
-		++it;
+		MutantStack<int> mstack;
+
+		std::cout << "push 5" << std::endl;
+		mstack.push(5);
+		std::cout << "push 17" << std::endl;
+		mstack.push(17);
+
+		std::cout << "top: " << mstack.top() << std::endl;
+
+		std::cout << "pop" << std::endl;
+		mstack.pop();
+
+		std::cout << "size: " << mstack.size() << std::endl;
+
+		std::cout << "push 3" << std::endl;
+		mstack.push(3);
+		std::cout << "push 5" << std::endl;
+		mstack.push(5);
+		std::cout << "push 737" << std::endl;
+		mstack.push(737);
+		std::cout << "push 0" << std::endl;
+		mstack.push(0);
+
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		std::cout << "MutantStack: ";
+		while (it != ite)
+		{
+			std::cout << *it << " ";
+			++it;
+		}
+		std::cout << std::endl;
+
+		std::cout << "MutantStack: ";
+		for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "MutantStack: ";
+		for (MutantStack<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "MutantStack: ";
+		for (MutantStack<int>::const_iterator it = mstack.begin(); it != mstack.end(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "MutantStack: ";
+		for (MutantStack<int>::const_reverse_iterator it = mstack.rbegin(); it != mstack.rend(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::stack<int> s(mstack);
 	}
-	std::cout << mstack.size() << std::endl;
-	std::stack<int> s(mstack);
-	
+
 	std::cout << std::endl;
 
-	std::cout << "List:" << std::endl;
-	std::list<int> list;
-	list.push_back(5);
-	list.push_back(17);
-	std::cout << list.back() << std::endl;
-	list.pop_back();
-	std::cout << list.size() << std::endl;
-	list.push_back(3);
-	list.push_back(5);
-	list.push_back(737);
-	list.push_back(0);
-	std::list<int>::iterator it2 = list.begin();
-	std::list<int>::iterator ite2 = list.end();
-	++it2;
-	--it2;
-	while (it2 != ite2)
 	{
-		std::cout << *it2 << std::endl;
-		++it2;
+		std::list<int> l;
+
+		std::cout << "push 5" << std::endl;
+		l.push_back(5);
+		std::cout << "push 17" << std::endl;
+		l.push_back(17);
+
+		std::cout << "top: " << l.back() << std::endl;
+
+		std::cout << "pop" << std::endl;
+		l.pop_back();
+
+		std::cout << "size: " << l.size() << std::endl;
+
+		std::cout << "push 3" << std::endl;
+		l.push_back(3);
+		std::cout << "push 5" << std::endl;
+		l.push_back(5);
+		std::cout << "push 737" << std::endl;
+		l.push_back(737);
+		std::cout << "push 0" << std::endl;
+		l.push_back(0);
+
+		std::list<int>::iterator it = l.begin();
+		std::list<int>::iterator ite = l.end();
+
+		std::cout << "std::list: ";
+		while (it != ite)
+		{
+			std::cout << *it << " ";
+			++it;
+		}
+		std::cout << std::endl;
+
+		std::cout << "std::list: ";
+		for (std::list<int>::iterator it = l.begin(); it != l.end(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "std::list: ";
+		for (std::list<int>::reverse_iterator it = l.rbegin(); it != l.rend(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "std::list: ";
+		for (std::list<int>::const_iterator it = l.begin(); it != l.end(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
+
+		std::cout << "std::list: ";
+		for (std::list<int>::const_reverse_iterator it = l.rbegin(); it != l.rend(); it++)
+			std::cout << *it << " ";
+		std::cout << std::endl;
 	}
-	std::cout << list.size() << std::endl;
 	return 0;
 }
