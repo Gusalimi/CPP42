2aaf55f860be29f36854202a50e423823361e20b
Guillaume Saile <gsaile@k0r4p3.42mulhouse.fr>
2023-07-08 10:59:59 +0200

    [M09] Done

--- M09/ex00/main.cpp
+++ M09/ex00/main.cpp
@@ -14,75 +14,75 @@ int main(int argc, char **argv)
 {
 	try {
 		BitcoinExchange exchange("data.csv");
+		std::ifstream file(argv[1]);
+		std::string ignore;
+		std::string line;
+		std::string date;
+		std::string value;
+		double value_d;
+		double convert;
+
+		if (argc != 2)
+		{
+			std::cout << "Usage: ./bitcoin [input]" << std::endl;
+			return (1);
+		}
+
+		if (!file.is_open())
+			throw std::runtime_error("Could not open file");
+		std::getline(file, line);
+		while (std::getline(file, line))
+		{
+			if (line.empty())
+				continue ;
+			std::stringstream ss(line);
+			if (!std::getline(ss, date, ' '))
+			{
+				std::cout << "Error: Bad input => " << line << std::endl;
+				continue ;
+			}
+			if (!isValidDateFormat(date) || std::stoi(date.substr(5, 2)) > 12 || std::stoi(date.substr(8, 2)) > 31)
+			{
+				std::cout << "Error: invalid date => " << date << std::endl;
+				continue ;
+			}
+			std::getline(ss, ignore, '|');
+			std::getline(ss, ignore, ' ');
+			if (!std::getline(ss, value))
+			{
+				std::cout << "Error: Bad input => " << line << std::endl;
+				continue ;
+			}
+			try
+			{
+				value_d = std::stod(value);
+			}
+			catch (std::exception &e)
+			{
+				std::cout << "Error: too large a number." << std::endl;
+				continue ;
+			}
+			if (value_d < 0)
+			{
+				std::cout << "Error: negative number." << std::endl;
+				continue ;
+			}
+			if (value_d > 1000)
+			{
+				std::cout << "Error: too large a number." << std::endl;
+				continue ;
+			}
+			try {
+				convert = exchange.convert(date, value_d);
+			} catch (std::exception &e) {
+				std::cout << "Error: " << e.what() << std::endl;
+				continue ;
+			}
+			std::cout << date << " => " << value << " = " << exchange.convert(date, std::stod(value)) << std::endl;
+		}
+		return 0;
 	} catch (std::exception &e) {
 		std::cout << "Error: " << e.what() << std::endl;
-		return 1;
+		return (1);
 	}
-	std::ifstream file(argv[1]);
-	std::string ignore;
-	std::string line;
-	std::string date;
-	std::string value;
-	double value_d;
-	double convert;
-
-	if (argc != 2)
-	{
-		std::cout << "Usage: ./bitcoin [input]" << std::endl;
-		return 1;
-	}
-
-	if (!file.is_open())
-		throw std::runtime_error("Could not open file");
-	std::getline(file, line);
-	while (std::getline(file, line))
-	{
-		if (line.empty())
-			continue ;
-		std::stringstream ss(line);
-		if (!std::getline(ss, date, ' '))
-		{
-			std::cout << "Error: Bad input => " << line << std::endl;
-			continue ;
-		}
-		if (!isValidDateFormat(date) || std::stoi(date.substr(5, 2)) > 12 || std::stoi(date.substr(8, 2)) > 31)
-		{
-			std::cout << "Error: invalid date => " << date << std::endl;
-			continue ;
-		}
-		std::getline(ss, ignore, '|');
-		std::getline(ss, ignore, ' ');
-		if (!std::getline(ss, value))
-		{
-			std::cout << "Error: Bad input => " << line << std::endl;
-			continue ;
-		}
-		try
-		{
-			value_d = std::stod(value);
-		}
-		catch (std::exception &e)
-		{
-			std::cout << "Error: too large a number." << std::endl;
-			continue ;
-		}
-		if (value_d < 0)
-		{
-			std::cout << "Error: negative number." << std::endl;
-			continue ;
-		}
-		if (value_d > 1000)
-		{
-			std::cout << "Error: too large a number." << std::endl;
-			continue ;
-		}
-		try {
-			convert = exchange.convert(date, value_d);
-		} catch (std::exception &e) {
-			std::cout << "Error: " << e.what() << std::endl;
-			continue ;
-		}
-		std::cout << date << " => " << value << " = " << exchange.convert(date, std::stod(value)) << std::endl;
-	}
-	return 0;
 }
